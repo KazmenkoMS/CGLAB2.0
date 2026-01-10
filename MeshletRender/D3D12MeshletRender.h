9cf718c80cbe1f4890bd1ed6ad6fffd5a7332ab8
m.kazmenko <michaelkazmenko@gmail.com>
2026-01-10 09:19:16 +0300

    added texture support
    added texture binding, everything else is the same

--- MeshletRender/D3D12MeshletRender.h
+++ MeshletRender/D3D12MeshletRender.h
@@ -81,7 +81,7 @@ private:
     ComPtr<ID3D12Fence> m_fence;
     UINT64 m_fenceValues[FrameCount];
 
-	bool bUseWireframe = false;
+    bool bUseWireframe = false;
 
     void LoadPipeline();
     void LoadAssets();
@@ -93,4 +93,17 @@ private:
     static const wchar_t* c_meshFilename;
     static const wchar_t* c_meshShaderFilename;
     static const wchar_t* c_pixelShaderFilename;
+
+    struct AlbedoBinding
+    {
+        Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> Heap; // shader-visible SRV heap (1 descriptor)
+        Microsoft::WRL::ComPtr<ID3D12Resource> Tex; // default heap texture
+        Microsoft::WRL::ComPtr<ID3D12Resource> Upload; // upload heap staging
+    };
+
+    AlbedoBinding m_albedo;
+    UINT m_cbvSrvUavInc = 0;
+
+    void InitAlbedoResources();
+    void BindAlbedoTexture();
 };
